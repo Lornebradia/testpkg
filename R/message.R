9d06fd3083932a47d4ae597afd5a5f7847908b0c
Lorenzo Braschi <lorenzo.braschi@roche.com>
2021-09-14 15:37:40 +0200

    Added function and test

---
+++ R/message.R
@@ -0,0 +1,9 @@
+#' Message
+#'
+#' A function with a message
+#'
+#' @param name A string
+#' @export
+message <- function(name) {
+  paste0("Hello, ", name)
+}
---
+++ tests/testthat/test-message.R
@@ -0,0 +1,3 @@
+test_that("multiplication works", {
+  expect_equal(message("Lorenzo"), "Hello, Lorenzo")
+})
